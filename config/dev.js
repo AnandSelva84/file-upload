088d155a46d91c15cae5ee295c516c29a1dae759
Selvaraj <Anand.Selvaraj@thomsonreuters.com>
2019-05-09 17:39:35 -0500

    first-commit

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    AWS_SECRET_ACCESS: process.env.AWS_SECRET,
+    AWS_ACCESS_KEY: process.env.AWS_KEY
+  }
---
+++ server.js
@@ -0,0 +1,29 @@
+const express       = require("express"),
+      app           = express(),
+      bodyParser    = require("body-parser"),
+      config        = require("./config"),
+      path          = require("path");
+
+const fileRoutes    = require("./routes/file-upload");
+
+app.use(bodyParser.json()); // use od body parser to get values from get req
+
+
+app.use("/api/v1/", fileRoutes);
+
+
+if (process.env.NODE_ENV == 'production') {
+    console.log("Prod");
+  const appPath = path.join(__dirname, "..", "dist");
+  app.use(express.static(appPath));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || '3001';
+
+app.listen(PORT, function(){
+    console.log("Node server started on port " + PORT);
+});
